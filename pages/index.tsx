995563bb89200eb31bd94a557c54649066d33cde
sumana2001 <sumana.basu2001@gmail.com>
2022-02-02 19:12:10 +0530

    Minor fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Home: NextPage = () => {
   return (
