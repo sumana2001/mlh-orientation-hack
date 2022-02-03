2044b8bee1cc6f2d96796733e495cb74c2725f0d
Samson Amaugo <sammychinedu2ky@gmail.com>
2022-02-03 20:29:46 +0100

    style edit

--- Components/UserList.tsx
+++ Components/UserList.tsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react'
 import { DataContext } from '../pages'
 import User from './User'
+import styles from '../styles/Home.module.css'
 export default function UserList() {
     let context = useContext(DataContext)
     return (
         <>
-           <div>{context.state?.name}</div>
+           <div className={styles.topic}>{context.state?.name}</div>
             {context.state?.info?.map((user: any, key: any) => <User key={key} data={user} />)}
         </>
     )
