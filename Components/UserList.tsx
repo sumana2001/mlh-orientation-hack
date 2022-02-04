4d4c10353338ec577daa69118b870dba83d961a3
sumana2001 <sumana.basu2001@gmail.com>
2022-02-04 11:26:51 +0530

    Styling

--- Components/UserList.tsx
+++ Components/UserList.tsx
@@ -6,10 +6,15 @@ export default function UserList() {
     let context = useContext(DataContext)
     return (
         <>
-           <div className={styles.name}>{context.state?.name}</div>
+           <div className={styles.name}>
+               {context.state?.name}
+           </div>
            <div className={styles.url}>{context.state?.url}</div>
            <div className={styles.count}>{context.state?.stargazerCount}</div>
+           <br/>
+           <div className={styles.grid}>
             {context.state?.info?.map((user: any, key: any) => <User key={key} data={user} />)}
+            </div>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,15 +13,15 @@ import {
 
 interface IDataContext {
   state?:{
-    name?:String,
-    url?:String,
+    name?:string,
+    url?:string,
     stargazerCount?:Number,
     info?:[]
   },
   dispatch?: ({type,data}:any)=>void
 }
 
-const reducer = (state:{name?:String,url?:String,stargazerCount?:Number,info?:[]}, action:any) => {
+const reducer = (state:{name?:string,url?:string,stargazerCount?:Number,info?:[]}, action:any) => {
   switch (action.type) {
     case "DATA":
       let newState ={
